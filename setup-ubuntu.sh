7a207526be7a31f23d92e672e385e7d1bb06debd
Marco <marco.bretzke@gmail.com>
2017-11-12 22:29:43 +0100

    Update setup-ubuntu.sh
    added repo for i3-gaps on ubuntu 16.04

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -106,6 +106,10 @@ mkdir i3-source
 cd i3-source/
 git clone https://www.github.com/Airblader/i3 i3-gaps
 cd i3-gaps/
+sudo add-apt-repository ppa:aguignard/ppa -y
+sudo apt-get update -y
+sudo apt-get install libxcb-xrm-dev -y
+
 sudo apt-get install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm-dev
 autoreconf --force --install
 rm -rf build/
